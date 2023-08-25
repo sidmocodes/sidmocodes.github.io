4b181d3c6aa07fd69189688a399e500ee74dc1a5
Sid <47861446+sidmocodes@users.noreply.github.com>
2023-08-25 17:33:54 +0530

    Update scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -29,6 +29,42 @@ document.addEventListener("DOMContentLoaded", function() {
             this.style.transform = 'scale(1)';
         });
     });
+
+    // Smooth Fade In effect as user scrolls
+    window.addEventListener('scroll', function() {
+        let scrollPosition = window.scrollY;
+        document.querySelectorAll('.fade-in').forEach(element => {
+            let elementPosition = element.getBoundingClientRect().top + window.innerHeight * 0.25;
+            if (scrollPosition > elementPosition) {
+                element.style.opacity = '1';
+                element.style.transform = 'translateY(0)';
+            }
+        });
+    });
+
+    // Sticky Navigation Bar
+    const navbar = document.querySelector('nav');
+    const sticky = navbar.offsetTop;
+    window.addEventListener('scroll', function() {
+        if (window.pageYOffset >= sticky) {
+            navbar.classList.add('sticky');
+        } else {
+            navbar.classList.remove('sticky');
+        }
+    });
+
+    // Scroll to Top Button
+    const scrollToTopButton = document.getElementById('scrollToTop');
+    window.addEventListener('scroll', function() {
+        if (window.pageYOffset > 200) {
+            scrollToTopButton.style.display = 'block';
+        } else {
+            scrollToTopButton.style.display = 'none';
+        }
+    });
+    scrollToTopButton.addEventListener('click', function() {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    });
 });
 
 // Optional: Add additional JS logic as per your requirements
