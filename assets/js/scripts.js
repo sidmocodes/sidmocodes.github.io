845a5b9143013f4079233f033df7718db2bec6b5
sidmocodes <sidmo.dev@gmail.com>
2023-08-25 19:52:31 +0530

    Update CSS

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,45 +1,59 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Get all the anchor links in the navigation
-    const navLinks = document.querySelectorAll('nav a');
-
-    navLinks.forEach(link => {
-        link.addEventListener('click', function(event) {
-            // Prevent the default anchor link behavior
-            event.preventDefault();
-
-            // Get the target section's top offset
-            const targetId = this.getAttribute('href');
-            const targetSection = document.querySelector(targetId);
-            const targetPosition = targetSection.offsetTop;
-
-            // Scroll to the target section with animation
-            window.scrollTo({
-                top: targetPosition-10,
-                behavior: 'smooth'
-            });
-        });
-    });
-});
-
-fetch('/api/blog')
-    .then(response => response.json())
-    .then(blogs => {
-        const blogList = document.getElementById('blog-list');
-        blogs.forEach(blog => {
-            const blogItem = document.createElement('li');
-            blogItem.textContent = blog.title;
-            blogList.appendChild(blogItem);
+document.addEventListener("DOMContentLoaded", function() {
+    // Smooth scrolling for anchor links
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function(e) {
+            e.preventDefault();
+            const target = document.querySelector(this.getAttribute('href'));
+            if (target) {
+                target.scrollIntoView({ behavior: 'smooth', block: 'start' });
+            }
         });
     });
 
-window.addEventListener('scroll', function() {
-    const heading = document.getElementById('projects-heading');
-    const top = heading.getBoundingClientRect().top;
-    const windowHeight = window.innerHeight;
+    // Project card hover effect
+    document.querySelectorAll('.card').forEach(card => {
+        card.addEventListener('mouseenter', function() {
+            this.style.transform = 'scale(1.05)';
+        });
+        card.addEventListener('mouseleave', function() {
+            this.style.transform = 'scale(1)';
+        });
+    });
 
-    if (top < windowHeight / 2) {
-        heading.classList.add('bounce');
-    } else {
-        heading.classList.remove('bounce');
-    }
+    // Icon pop-out effect on hover
+    document.querySelectorAll('.fab').forEach(icon => {
+        icon.addEventListener('mouseenter', function() {
+            this.style.transform = 'scale(1.2)';
+        });
+        icon.addEventListener('mouseleave', function() {
+            this.style.transform = 'scale(1)';
+        });
+    });
+
+    // Smooth Fade In effect as user scrolls
+    window.addEventListener('scroll', function() {
+        let scrollPosition = window.scrollY;
+        document.querySelectorAll('.fade-in').forEach(element => {
+            let elementPosition = element.getBoundingClientRect().top + window.innerHeight * 0.25;
+            if (scrollPosition > elementPosition) {
+                element.style.opacity = '1';
+                element.style.transform = 'translateY(0)';
+            }
+        });
+    });
+
+    // Scroll to Top Button
+    const scrollToTopButton = document.getElementById('scrollToTop');
+    window.addEventListener('scroll', function() {
+        if (window.pageYOffset > 200) {
+            scrollToTopButton.style.display = 'block';
+        } else {
+            scrollToTopButton.style.display = 'none';
+        }
+    });
+    scrollToTopButton.addEventListener('click', function() {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    });
 });
+
+// Optional: Add additional JS logic as per your requirements
