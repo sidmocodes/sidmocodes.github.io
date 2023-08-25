67cbd31a2a82260efb4fb0b7805c7b1fc332437a
sidmocodes <sidmo.dev@gmail.com>
2023-08-26 00:55:47 +0530

    Add TODOs

---
+++ assets/todos.groovy
@@ -0,0 +1,4 @@
+//Add TODOs here
+TODO : Feature to separate content from the code, i.e. store content in a different file instead of index.html.
+TODO : Color enhancements, color schema and stuff, study some basic color wheel and triads.
+TODO : Paginate any different kind of content (articles, pics, whatever) if/when needed to be added.
