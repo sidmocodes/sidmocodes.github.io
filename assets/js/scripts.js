4e1f286f5df93f3398d8cc6ab84c5b021dd093e2
sidmocodes <sidmo.dev@gmail.com>
2023-08-25 19:26:04 +0530

    Update scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,70 +1,45 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // Smooth scrolling for anchor links
-    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-        anchor.addEventListener('click', function(e) {
-            e.preventDefault();
-            const target = document.querySelector(this.getAttribute('href'));
-            if (target) {
-                target.scrollIntoView({ behavior: 'smooth', block: 'start' });
-            }
-        });
-    });
+document.addEventListener('DOMContentLoaded', function() {
+    // Get all the anchor links in the navigation
+    const navLinks = document.querySelectorAll('nav a');
 
-    // Project card hover effect
-    document.querySelectorAll('.card').forEach(card => {
-        card.addEventListener('mouseenter', function() {
-            this.style.transform = 'scale(1.05)';
-        });
-        card.addEventListener('mouseleave', function() {
-            this.style.transform = 'scale(1)';
-        });
-    });
+    navLinks.forEach(link => {
+        link.addEventListener('click', function(event) {
+            // Prevent the default anchor link behavior
+            event.preventDefault();
 
-    // Icon pop-out effect on hover
-    document.querySelectorAll('.fab').forEach(icon => {
-        icon.addEventListener('mouseenter', function() {
-            this.style.transform = 'scale(1.2)';
-        });
-        icon.addEventListener('mouseleave', function() {
-            this.style.transform = 'scale(1)';
-        });
-    });
+            // Get the target section's top offset
+            const targetId = this.getAttribute('href');
+            const targetSection = document.querySelector(targetId);
+            const targetPosition = targetSection.offsetTop;
 
-    // Smooth Fade In effect as user scrolls
-    window.addEventListener('scroll', function() {
-        let scrollPosition = window.scrollY;
-        document.querySelectorAll('.fade-in').forEach(element => {
-            let elementPosition = element.getBoundingClientRect().top + window.innerHeight * 0.25;
-            if (scrollPosition > elementPosition) {
-                element.style.opacity = '1';
-                element.style.transform = 'translateY(0)';
-            }
+            // Scroll to the target section with animation
+            window.scrollTo({
+                top: targetPosition-10,
+                behavior: 'smooth'
+            });
         });
-    });
-
-    // Sticky Navigation Bar
-    const navbar = document.querySelector('nav');
-    const sticky = navbar.offsetTop;
-    window.addEventListener('scroll', function() {
-        if (window.pageYOffset >= sticky) {
-            navbar.classList.add('sticky');
-        } else {
-            navbar.classList.remove('sticky');
-        }
-    });
-
-    // Scroll to Top Button
-    const scrollToTopButton = document.getElementById('scrollToTop');
-    window.addEventListener('scroll', function() {
-        if (window.pageYOffset > 200) {
-            scrollToTopButton.style.display = 'block';
-        } else {
-            scrollToTopButton.style.display = 'none';
-        }
-    });
-    scrollToTopButton.addEventListener('click', function() {
-        window.scrollTo({ top: 0, behavior: 'smooth' });
     });
 });
 
-// Optional: Add additional JS logic as per your requirements
+fetch('/api/blog')
+    .then(response => response.json())
+    .then(blogs => {
+        const blogList = document.getElementById('blog-list');
+        blogs.forEach(blog => {
+            const blogItem = document.createElement('li');
+            blogItem.textContent = blog.title;
+            blogList.appendChild(blogItem);
+        });
+    });
+
+window.addEventListener('scroll', function() {
+    const heading = document.getElementById('projects-heading');
+    const top = heading.getBoundingClientRect().top;
+    const windowHeight = window.innerHeight;
+
+    if (top < windowHeight / 2) {
+        heading.classList.add('bounce');
+    } else {
+        heading.classList.remove('bounce');
+    }
+});
